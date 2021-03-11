0149dbeb4e2f47b049a7a8626ab9db71daa27b79
piglig <zhu1197437384@gmail.com>
2021-03-11 23:41:19 +0800

    pcache

--- lru/lru.go
+++ lru/lru.go
@@ -54,4 +54,23 @@ func (c *Cache) RemoveOldest() {
 	}
 }
 
-func (c *Cache) Add \\
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nBytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nBytes += int64(len(key)) + int64(value.Len())
+	}
+
+	for c.maxBytes != 0 && c.maxBytes < c.nBytes {
+		c.RemoveOldest()
+	}
+}
+
+func (c *Cache) Len() int {
+	return c.ll.Len()
+}
