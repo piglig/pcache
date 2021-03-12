7e376c86f53b03c74c3ddcc3bcc4d499204ec90e
zhuzhenwu <zhuzhenwu@gg.com>
2021-03-12 19:08:56 +0800

    ✨feat(pcache):

---
+++ byteview.go
@@ -0,0 +1,24 @@
+package pcache
+
+type ByteView struct {
+	b []byte
+}
+
+// Len return the view's length
+func (v ByteView) Len() int { return len(v.b) }
+
+// ByteSlice returns a copy of the data as a slice of bytes
+func (v ByteView) ByteSlice() []byte {
+	return cloneBytes(v.b)
+}
+
+// String returns the data as a string, making a copy if necessary
+func (v ByteView) String() string {
+	return string(v.b)
+}
+
+func cloneBytes(b []byte) []byte {
+	c := make([]byte, len(b))
+	copy(c, b)
+	return c
+}
--- lru/lru_test.go
+++ lru/lru_test.go
@@ -1,6 +1,7 @@
 package lru
 
 import "testing"
+import "reflect"
 
 type String string
 
@@ -20,5 +21,32 @@ func TestGet(t *testing.T) {
 func TestRemoveOldest(t *testing.T) {
 	k1, k2, k3 := "key1", "key2", "key3"
 	v1, v2, v3 := "value1", "value2", "value3"
-	cap := len
+	cap := len(k1 + k2 + v1 + v2)
+	lru := New(int64(cap), nil)
+	lru.Add(k1, String(v1))
+	lru.Add(k2, String(v2))
+	lru.Add(k3, String(v3))
+
+	if _, ok := lru.Get("key1"); ok || lru.Len() != 2 {
+		t.Fatalf("RemoveOldest key1 failed")
+	}
+}
+
+func TestOnEvicted(t *testing.T) {
+	keys := make([]string, 0)
+	callback := func(key string, value Value) {
+		keys = append(keys, key)
+	}
+
+	lru := New(int64(10), callback)
+	lru.Add("key1", String("123456"))
+	lru.Add("k2", String("k2"))
+	lru.Add("k3", String("k3"))
+	lru.Add("k4", String("k4"))
+
+	expect := []string{"key1", "k2"}
+
+	if !reflect.DeepEqual(expect, keys) {
+		t.Fatalf("Call OnEvicted failed, expect keys equals to %s", expect)
+	}
 }
